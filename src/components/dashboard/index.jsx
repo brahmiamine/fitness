673ee6c9f1d054f5a4085fab43f0976e658537aa
brahmiamine <ybrahmi66@gmail.com>
2026-09-20 12:01:46 +0200

    Remove obsolete dashboard view plumbing

--- src/components/dashboard/index.jsx
+++ src/components/dashboard/index.jsx
@@ -1,7 +1,5 @@
-import { summarizeDay } from '../../lib/analysis'
 import { DecisionsView } from './DecisionsView'
 
 export function Dashboard({ dataset, day, history = [] }) {
-  summarizeDay(dataset, day)
   return <DecisionsView dataset={dataset} day={day} history={history} />
 }
